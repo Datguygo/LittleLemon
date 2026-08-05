59f17af2c5e9f7db1e09a31bc25f2d62c6f89e1c
Anthony High <anthony.high1@gmail.com>
2026-08-04 21:56:20 -0500

    Added API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu/items', views.MenuItemsView.as_view()),
+    path('menu/items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
